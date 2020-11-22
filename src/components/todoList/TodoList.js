c739891f40fb9ef911b2018b88d56d1d56e6e223
EvgeniBR <genib1989@gmail.com>
2020-11-22 14:40:18 +0200

    first commit

---
+++ src/components/todoList/TodoList.js
@@ -0,0 +1,65 @@
+import React, { useEffect, useState } from "react";
+import Todo from "../todo/Todo";
+import TodoForm from "../todoForm/TodoForm";
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    if (JSON.parse(localStorage.getItem(`todo`))) {
+      setTodos(JSON.parse(localStorage.getItem(`todo`)));
+    }
+  }, []);
+
+  const addTodo = (todo) => {
+    if (!todo.text || /^\s*$/.test(todo.text)) {
+      return;
+    }
+
+    const newTodos = [todo, ...todos];
+
+    localStorage.setItem(`todo`, JSON.stringify(newTodos));
+    setTodos(newTodos);
+  };
+
+  const updateTodo = (todoId, newValue) => {
+    if (!newValue.text || /^\s*$/.test(newValue.text)) {
+      return;
+    }
+
+    setTodos((prev) =>
+      prev.map((item) => (item.id === todoId ? newValue : item))
+    );
+  };
+
+  const removeTodo = (id) => {
+    const removedArr = [...todos].filter((todo) => todo.id !== id);
+    localStorage.setItem(`todo`, JSON.stringify(removedArr));
+    setTodos(removedArr);
+  };
+
+  const completeTodo = (id) => {
+    let updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        todo.isComplete = !todo.isComplete;
+      }
+      return todo;
+    });
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <>
+      <TodoForm onSubmit={addTodo} />
+      <h1>Todos for Climacell</h1>
+      <Todo
+        todos={todos}
+        completeTodo={completeTodo}
+        removeTodo={removeTodo}
+        updateTodo={updateTodo}
+      />
+    </>
+  );
+}
+
+export default TodoList;
